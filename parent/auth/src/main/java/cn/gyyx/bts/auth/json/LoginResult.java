bc8a91546a2a92019f28c79cfdf4b37f5ff154b3
ssstzbs <ssst_zbs@126.com>
2019-08-13 22:22:07 +0800

    (zbs) add LoginResult.java;

---
+++ parent/auth/src/main/java/cn/gyyx/bts/auth/json/LoginResult.java
@@ -0,0 +1,13 @@
+package cn.gyyx.bts.auth.json;
+
+public class LoginResult {
+	private String code;
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+}
