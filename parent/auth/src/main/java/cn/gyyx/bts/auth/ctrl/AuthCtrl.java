c8f4d53939e5863aa4509aba2e07dc205b7ad630
ssstzbs <ssst_zbs@126.com>
2019-08-15 15:40:13 +0800

    (17.33) add LoginResultc.java and called!

--- parent/auth/src/main/java/cn/gyyx/bts/auth/ctrl/AuthCtrl.java
+++ parent/auth/src/main/java/cn/gyyx/bts/auth/ctrl/AuthCtrl.java
@@ -2,10 +2,15 @@ package cn.gyyx.bts.auth.ctrl;
 
 import com.google.inject.Inject;
 
+import cn.gyyx.bts.auth.json.LoginResultc;
+
 public class AuthCtrl {
 
+	private LoginResultc loginResultc;
+	
 	@Inject
 	public AuthCtrl() {
-		
+		loginResultc = new LoginResultc();
+		loginResultc.setCode("111");
 	}
 }
